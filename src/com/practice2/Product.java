f937b3665c4c0682408715254efdda25a1fab561
DipeshTalekar <talekardipesh2000@gmail.com>
2024-02-20 20:51:06 +0530

    Commit for 19-02-2024

---
+++ src/com/practice2/Product.java
@@ -0,0 +1,45 @@
+package com.practice2;
+
+public class Product {
+    private String productId;
+    private String productName;
+    private double price;
+
+    public Product(String productId,String productName,double price){
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+    }
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId='" + productId + '\'' +
+                ", productName='" + productName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
